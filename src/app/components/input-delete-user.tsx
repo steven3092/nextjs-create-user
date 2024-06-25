4e9d79e55be4a32fee3f0e6086086d57cd0c7eab
steven fauterre <steven.fauterre@wefox.com>
2024-06-25 17:14:07 +0200

    create input to delete user

---
+++ src/app/components/input-delete-user.tsx
@@ -0,0 +1,16 @@
+import { useDeleteUsers } from "../hooks/use-delete-users";
+
+export const InputDeleteUser = ({ userId }: { userId: number }) => {
+  const { handleDeleteUser } = useDeleteUsers(userId);
+
+  const handleOnClickDeleteUser = async () => {
+    await handleDeleteUser();
+    return;
+  };
+
+  return (
+    <>
+      <input value="delete" type="button" onClick={handleOnClickDeleteUser} />
+    </>
+  );
+};
