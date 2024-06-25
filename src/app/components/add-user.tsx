35c637a4ca62b467ed1867922a8e3bf0047f6af8
steven fauterre <steven.fauterre@wefox.com>
2024-06-25 17:13:47 +0200

    create add user form

---
+++ src/app/components/add-user.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { useAddUser } from "../hooks/use-add-user";
+import { Button } from "./button";
+
+export const AddUser = () => {
+  const { handleSubmitForm } = useAddUser();
+
+  return (
+    <>
+      <form onSubmit={handleSubmitForm}>
+        <label>First name :</label>
+        <input type="text" name="firstName" />
+        Last Name :
+        <input type="text" name="lastName" />
+        <Button name="Submit" type="submit" />
+      </form>
+    </>
+  );
+};
