549c60fc351500a3134cdd3b8263e928cf38ac27
steven fauterre <steven.fauterre@wefox.com>
2024-06-25 17:12:36 +0200

    delete files

--- src/app/api/requests.tsx
+++
@@ -1,8 +0,0 @@
-export async function fetchData() {
-  const response = await fetch(
-    "https://mdn.github.io/dom-examples/fetch/fetch-json/products.json"
-  );
-  const products = await response.json();
-
-  return products;
-}
--- src/app/form/actions.tsx
+++
@@ -1,6 +0,0 @@
-"use server";
-
-export async function addUser(state: any, formData: FormData) {
-  // Mutate data
-  console.log(`user ${formData.get("firstName")} added`);
-}
