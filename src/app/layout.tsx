438d2e965b6a17700b542cd6fa76223737bd8759
steven fauterre <steven.fauterre@wefox.com>
2024-07-01 17:52:13 +0200

    msw config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,29 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
-import Provider from "./utils/provider";
+import ReactQueryProvider from "./utils/react-query-provider";
+// import { server } from "./mocks/node";
+// import { MockProvider } from "./utils/mock-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  // if (process.env.NODE_ENV == "development") {
+  //   server.listen();
+
+  //   const response = await fetch("http://127.0.0.1:8000/users");
+  //   const user = await response.json();
+  //   console.log(user);
+  // }
+
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Provider>{children}</Provider>
+        <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
     </html>
   );
