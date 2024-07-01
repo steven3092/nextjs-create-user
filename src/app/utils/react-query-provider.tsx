2aec449bf79574bd9ac45d413bedc584fd3e8975
steven fauterre <steven.fauterre@wefox.com>
2024-07-01 17:51:40 +0200

    rename

---
+++ src/app/utils/react-query-provider.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { QueryClient } from "@tanstack/query-core";
+import { QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import { ReactNode } from "react";
+
+export default function ReactQueryProvider({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const [queryClient] = React.useState(() => new QueryClient());
+
+  return (
+    <>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </>
+  );
+}
