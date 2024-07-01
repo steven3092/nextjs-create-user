d1fa7193b6ecb5e485addef9daa1a52cf1cc0be2
steven fauterre <steven.fauterre@wefox.com>
2024-07-01 17:53:31 +0200

    reset form when submission

--- src/app/hooks/use-add-users/use-add-users.tsx
+++ src/app/hooks/use-add-users/use-add-users.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { FormEvent } from "react";
 import { usePostUsers } from "../use-post-users/use-post-users";
 
@@ -5,12 +6,16 @@ export const useAddUsers = () => {
   const { createUserMutation } = usePostUsers();
 
   const handleSubmitForm = async (event: FormEvent<HTMLFormElement>) => {
+    const form = document?.getElementById("userForm") as HTMLFormElement;
     event.preventDefault();
 
     const formData = new FormData(event.target as HTMLFormElement);
 
     await createUserMutation.mutateAsync(formData, {
-      onSuccess: () => console.log("User created"),
+      onSuccess: () => {
+        console.log("User created");
+        form?.reset();
+      },
     });
   };
 
